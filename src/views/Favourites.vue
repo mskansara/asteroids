977f4c9fe89fce25844d30118f5a1e42d585cc66
Manthan Kansara <manthankansara7@gmail.com>
2020-10-17 21:24:12 +0530

    Added firestore for adding favourites

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -1,5 +1,35 @@
 <template>
     <div class="favourite container">
         <h3>Favorites will be shown here</h3>
+        <p v-for="item in data" :key=item.id>{{item.user_id}}</p>
     </div>
 </template>
+
+
+<script>
+    import firebase from 'firebase';
+    export default {
+        data () {
+            return {
+                data: []
+            }
+        },
+        created() {
+            let user = firebase.auth().currentUser;
+            const db = firebase.firestore();
+            db.collection("asteroids")
+            .where('user_id', '==' ,user.uid)
+            .get()
+            .then((response) => {
+                response.forEach(element => {
+                    console.log(element.data());
+                    this.data.push(element.data());
+                });
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+        }
+    }
+</script>>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,7 @@
             <td>Min:{{item.estimated_diameter.kilometers.estimated_diameter_min}}, Max:{{item.estimated_diameter.kilometers.estimated_diameter_max}}</td>
             <td>Min:{{item.estimated_diameter.miles.estimated_diameter_min}}, Max:{{item.estimated_diameter.miles.estimated_diameter_max}}</td>
             <td>{{item.orbital_data.data_arc_in_days}}</td>
-            <td><button class="btn"><i class="material-icons">add</i></button></td>
+            <td><button class="btn" @click="addToFavourite(item)"><i class="material-icons">add</i></button></td>
         </tr>
       </tbody>
     </table>
@@ -38,6 +38,7 @@
 
 <script>
 // @ is an alias to /src
+  import firebase from 'firebase';
   export default {
     name: 'Home',
     data () {
@@ -62,9 +63,41 @@
       let url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?page=0&size=10&api_key=Iy2sbpgTZ70mVFS7Hki3hjWbyMDc3jeSrMfgdrIu'
       window.axios.get(url)
       .then((response) => {
-        console.log(response.data.near_earth_objects)
         this.data = response.data.near_earth_objects
       })
+    },
+    methods: {
+      addToFavourite(item) {
+        var user = firebase.auth().currentUser;
+        if(user) {
+          const db = firebase.firestore();
+          db.collection("asteroids")
+          .add({
+            user_id: user.uid,
+            asteroid_id: item.id,
+            is_potentially_hazardous: item.is_potentially_hazardous_asteroid,
+            neo_reference_id: item.neo_reference_id,
+            designation: item.designation,
+            name: item.name,
+            estimated_diameter_kilometer_min: item.estimated_diameter.kilometers.estimated_diameter_min,
+            estimated_diameter_kilometer_max: item.estimated_diameter.kilometers.estimated_diameter_max,
+            estimated_diameter_miles_min: item.estimated_diameter.miles.estimated_diameter_min,
+            estimated_diameter_miles_max: item.estimated_diameter.miles.estimated_diameter_max,
+            estimated_diameter_feet_min: item.estimated_diameter.feet.estimated_diameter_min,
+            estimated_diameter_feet_max: item.estimated_diameter.feet.estimated_diameter_max,
+            is_sentry_object: item.is_sentry_object,
+            data_arc_in_days: item.orbital_data.data_arc_in_days
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+        } else {
+          alert("You need to log in first")
+        }
+      }
     }
   }
 </script>
